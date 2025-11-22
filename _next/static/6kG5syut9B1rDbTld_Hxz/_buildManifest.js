9e8d32970f6334cc00a01e580b1c26c9f438464a
Tiago Selpuveda <tiagofoks@yahoo.com.br>
2025-11-22 14:40:17 -0300

    Updates

--- _next/static/6kG5syut9B1rDbTld_Hxz/_buildManifest.js
+++ _next/static/6kG5syut9B1rDbTld_Hxz/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/4569a70633c202fa.js"
+    "static/chunks/bf12e3c43003b36d.js"
   ],
   "/_error": [
     "static/chunks/95effb84aab1eba7.js"
--- _next/static/chunks/10d849959c29dfbe.js
+++ _next/static/chunks/10d849959c29dfbe.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/b34d1de5b5105647.js",
+  "static/chunks/8890c8502b15a2d8.js",
   "static/chunks/78631fcec60d8d54.js",
   "static/chunks/a09781bec7a92a0d.css",
-  "static/chunks/turbopack-55d91530e64459ef.js"
+  "static/chunks/turbopack-72c40292a8db1ad7.js"
 ])
